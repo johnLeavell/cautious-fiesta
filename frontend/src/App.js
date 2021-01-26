453609ddc0a29eeff1db01693e42cb748c8d28b0
John <johnleavell@gmail.com>
2021-01-25 21:30:47 -0600

    building out home screeen and added images to project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen'; 
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-          <h1>Welcome to Imperfect</h1>
+          <HomeScreen/>
         </Container>
       </main>
       <Footer />
     </>
   );
-}
+};
 
 export default App;
