71d975fce908e69292675039586902cc6145c2d8
John Leavell <johnleavell@gmail.com>
2021-01-28 14:28:54 -0600

    rendered thumbnail images of products

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Image } from 'react-bootstrap';
 
 const Product = ({ product }) => {
   console.log(product);
   return (
-    <Card className="my-3 p-3 rounded">
+    <Card className="my-1 p-1 rounded">
       <a href={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant="top" />
+        <Image src={product.image} variant="top" thumbnail fluid />
       </a>
 
       <Card.Body>
         <a href={`/product/${product._id}`}>
           <Card.Title as="div">
-            <strong>{product.bankName}</strong>
+            <strong>Bank Name: {product.bankName}</strong>
           </Card.Title>
         </a>
 
         <Card.Text as="div">
-          <div className="my-3">{product.rating}</div>
+          <div className="my-1">Card ID: {product.cardId}</div>
         </Card.Text>
+
+        <Card.Text as="div">
+          <div className="my-1">Date Opened: {product.dateOpened}</div>
+        </Card.Text>
+
+        <Card.Text as="div">
+          <div className="my-1">Credit Limit: ${product.creditLimit.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
+        </Card.Text>
+
       </Card.Body>
+      
     </Card>
   );
 };
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Table } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 import products from '../products';
 import Product from '../components/Product';
 
@@ -7,13 +7,16 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Imperfect Supply List</h1>
+      <Container>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} lx={3}>
-            <Product product={product} />
+          <Col xs={6} sm={12} md={4} lg={4} lx={3}>
+            <Product product={product} key={product.id}/>
           </Col>
         ))}
       </Row>
+      </Container>
+      
     </>
   );
 };
